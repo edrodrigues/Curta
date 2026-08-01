81b69c862d14bdc3370bb678fb66c63f882520a9
Ed Rodrigues <ed.ufpe@gmail.com>
2026-08-01 16:00:55 -0300

    - Corrije Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@ffmpeg-installer/ffmpeg", "fluent-ffmpeg"],
+  },
 };
 
 export default nextConfig;
